600470b26cfed84b5dd6a65820d11b213dc19cd6
Michael Gulenko <mgulenko@hotmail.com>
2019-09-08 13:50:39 -0400

    Added social media bar

--- breezy-v1/src/home_page/MainSectionComponent.js
+++ breezy-v1/src/home_page/MainSectionComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './MainSectionComponent.css'
 import '..//index.css';
+import SocialMediaBar from '../shared_components/SocialMediaBarComponent.js'
+import { Orientation } from '../Definitions';
 
 const mainSectionComponent = (props)=>
 {
@@ -9,12 +11,21 @@ const mainSectionComponent = (props)=>
     {
         alignSelf: 'center'
     };
+   
+
     //generate component
     return (        
        <div className="mainSectionContainer"> 
-          <h1 className="mainSectionHeader">BREEZY LEATHER WRAPS</h1>
-          <p className="mainSectionContent">Handmade genuine gemstone leather stitch bracelets</p>
-          <button className="buttonOnPrimeLrg" style={buttonStyle}>LEARN MORE</button>
+            <div className="mainSectionWrapper">
+                <div className="mainSectionContentWrapper">
+                    <h1 className="mainSectionHeader">BREEZY LEATHER WRAPS</h1>
+                    <p className="mainSectionContent">Handmade genuine gemstone leather stitch bracelets</p>
+                    <button className="buttonOnPrimeLrg" style={buttonStyle}>LEARN MORE</button>
+                </div>
+                <div className="mainSocialMediaBarStyle">
+                    <SocialMediaBar orientation = {Orientation.Vertical}/>
+                </div>
+            </div>
        </div>
     );
 }
