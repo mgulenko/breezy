b881c97621d37ee6f0e07d1d23757c348f76f9e8
Michael Gulenko <mgulenko@hotmail.com>
2019-09-10 22:41:48 -0400

    Implemented nav bar

--- breezy-v1/src/home_page/MainSectionComponent.js
+++ breezy-v1/src/home_page/MainSectionComponent.js
@@ -3,6 +3,7 @@ import './MainSectionComponent.css'
 import '..//index.css';
 import SocialMediaBar from '../shared_components/SocialMediaBarComponent.js'
 import { Orientation } from '../Definitions';
+import NavBar from './NavigationBarComponent';
 
 const mainSectionComponent = (props)=>
 {
@@ -14,19 +15,23 @@ const mainSectionComponent = (props)=>
    
 
     //generate component
-    return (        
-       <div className="mainSectionContainer"> 
-            <div className="mainSectionWrapper">
-                <div className="mainSectionContentWrapper">
-                    <h1 className="mainSectionHeader">BREEZY LEATHER WRAPS</h1>
-                    <p className="mainSectionContent">Handmade genuine gemstone leather stitch bracelets</p>
-                    <button className="buttonOnPrimeLrg" style={buttonStyle}>LEARN MORE</button>
-                </div>
-                <div className="mainSocialMediaBarStyle">
-                    <SocialMediaBar orientation = {Orientation.Vertical}/>
+    return (
+        <div>
+            <NavBar/>
+            <div className="mainSectionContainer"> 
+                <div className="mainSectionWrapper">
+                    <div className="mainSectionContentWrapper">
+                        <h1 className="mainSectionHeader">BREEZY LEATHER WRAPS</h1>
+                        <p className="mainSectionContent">Handmade genuine gemstone leather stitch bracelets</p>
+                        <button className="buttonOnPrimeLrg" style={buttonStyle}>LEARN MORE</button>
+                    </div>
+                    <div className="mainSocialMediaBarStyle">
+                        <SocialMediaBar orientation = {Orientation.Vertical}/>
+                    </div>
                 </div>
             </div>
-       </div>
+       </div>        
+       
     );
 }
 
--- breezy-v1/src/home_page/NavigationBarComponent.js
+++ breezy-v1/src/home_page/NavigationBarComponent.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import '../index.css';
+import { companyLogo, Palette } from '../Definitions';
+import './NavigationBarComponent.css'
+
 
 const navBarComponent = (props)=>
 {
-   
+    const styleContainer =
+    {
+        color: Palette.OnPrimaryColor,
+    };
+
+    const styleOrderBtn =
+    {
+        borderColor : Palette.OnPrimaryColor,
+    };
+    
     //generate component
     return (        
-       <div>
 
+       <div className="mainNavBarContainer" style={styleContainer}>
+           <div className="mainNavBarLogo">{companyLogo()}</div>
+           <div className="mainNavBarItem mainNavBarRightAlign"><p>ART WORK</p></div>
+           <div className="mainNavBarItem"><p>MATERIALS</p></div>
+           <div className="mainNavBarItem"><p>ABOUT</p></div>
+           <div className="mainNavBarItem"><p>CONTACT</p></div>
+           <div className="mainNavBarItem mainNavBarPlaceOrder"style={styleOrderBtn}><p>PLACE ORDER</p></div>
        </div>
     );
 }
