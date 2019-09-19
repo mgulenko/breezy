5461c46f174de8827d23ddfe14212576b220d980
Michael Gulenko <mgulenko@hotmail.com>
2019-09-18 22:12:41 -0400

    Added blank contact page + state in app.js that keeps track of the current page

--- breezy-v1/src/App.js
+++ breezy-v1/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './home_page/HomePage.js'
+import ContactPage from './contact_page/ContactPage.js'
+import { PageType } from './Definitions';
 
-class App extends Component {
+class App extends Component 
+{
+  state = 
+  {
+    currentPage : PageType.HomePage
+  }
+
   render() 
   {
+
+    let page = null;
+    switch(this.state.currentPage)
+    {
+      case PageType.HomePage:
+        page = (<HomePage/>);
+        break;
+      case PageType.ContactPage:
+        page = (<ContactPage/>);
+        break;
+      default:
+          page = (<HomePage/>);
+          break;
+    }
     return (
       <div className="App">
-        <HomePage/>
+        {page}
       </div>
     );
   }
---
+++ breezy-v1/src/contact_page/ContactPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const mainPageComponent = (props)=>
+{
+    return(
+        <div>
+            Contact
+        </div>
+    );
+}
+
+export default mainPageComponent;
